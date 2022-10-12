e2c9d2df56c419be6401b6d05ea23bdaf27c96c5
Elian-beep <elianoliver101@gmail.com>
2022-10-12 20:49:54 -0300

    all components finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
+import { Button } from './components/Button';
+import { Heading } from './components/Heading';
 import { Text } from './components/Text';
+import { TextInput, TextInputInputProps } from './components/TextInput';
+import { Envelope } from 'phosphor-react';
 import './styles/global.css';
+import { Checkbox } from './components/Checkbox';
 
 export function App() {
 
   return (
     <>
+      <Text>Ainda vamos codar...</Text>
     </>
   )
 }
+
+
+
+
+
+
+
+
+
+
+// <div className='flex items-center gap-2'>
+//   <Checkbox />
+//   <Text>Lembrar de mim por 30 dias</Text>
+// </div>
+
+
+{/* <TextInput.Root>
+  <TextInput.Icon>
+    <Envelope />
+  </TextInput.Icon>
+  <TextInput.Input
+    placeholder='Type your e-mail addres'
+    type={'email'}
+  />
+</TextInput.Root> */}
+
+
+
+
+
 
 
 {/* <h1 className='font-bold text-2xl text-violet-500'>
